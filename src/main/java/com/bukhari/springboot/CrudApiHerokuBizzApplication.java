8f04d020d6efb5dd07f1924def1d73cf821a2fe8
khurrambukhari <94698695+khurrambukhari@users.noreply.github.com>
2023-03-18 01:20:25 +0500

    first add

---
+++ src/main/java/com/bukhari/springboot/CrudApiHerokuBizzApplication.java
@@ -0,0 +1,13 @@
+package com.bukhari.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiHerokuBizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiHerokuBizzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bukhari/springboot/CrudApiHerokuBizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bukhari.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiHerokuBizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
