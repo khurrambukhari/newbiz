daf63466c4a8266e12508f2a582ce3d6b197ac52
khurram-786 <khurram-786@che>
2023-03-22 04:26:08 +0000

    c in controller

--- src/main/java/com/bukhari/springboot/controller/HomeController.java
+++ src/main/java/com/bukhari/springboot/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	
-	@GetMapping("/welcome")
+	@GetMapping("/")
 	public String welcome() {
 		return "welcome to heroku app demo";
 	}
